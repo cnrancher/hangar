5ac98f1aa2a69239bb18b6ff41e148698f0d465b
Starry Wang <starry.wang@suse.com>
2022-11-28 18:20:05 +0800

    Update README & help message

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 image-tools
 ===========
+
+> WIP
+
+Usage
+-----
+
+```sh
+go build -o image-tools .
+./image-tools -h
+./image-tools mirror -h
+```
--- image-tools.go
+++ image-tools.go
@@ -22,10 +22,10 @@ func main() {
 	// mirror reads file from image-list.txt or stdin and mirror image from
 	// original repo to
 	mirrorCmd := flag.NewFlagSet("mirror", flag.ExitOnError)
-	mirrorFile := mirrorCmd.String("file", "", "the image list file")
-	mirrorArch := mirrorCmd.String("arch", "x86_64,arm64", "the ARCH list of images, seperate with ','")
-	mirrorSourceReg := mirrorCmd.String("source", "", "override the source registry")
-	mirrorDestReg := mirrorCmd.String("dest", "", "override the destination registry")
+	mirrorFile := mirrorCmd.String("f", "", "the image list file")
+	mirrorArch := mirrorCmd.String("a", "x86_64,arm64", "the ARCH list of images, seperate with ','")
+	mirrorSourceReg := mirrorCmd.String("s", "", "override the source registry")
+	mirrorDestReg := mirrorCmd.String("d", "", "override the destination registry")
 	mirrorDebug := mirrorCmd.Bool("debug", false, "debug mode")
 
 	switch os.Args[1] {
@@ -47,6 +47,7 @@ func main() {
 }
 
 func showHelp() {
-	fmt.Printf("Usage: %s <sub-command> <parameters>\n", os.Args[0])
-
+	fmt.Printf("Usage: %s <subcommand> <parameters>\n", os.Args[0])
+	fmt.Printf("Run '%s <subcommand> -h' for more info.\n", os.Args[0])
+	fmt.Printf("Subcommands: mirror\n")
 }
