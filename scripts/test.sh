7523db5eba1f570984322957a2c16d35883ea2eb
Starry Wang <starry.wang@suse.com>
2022-12-30 15:09:13 +0800

    Update test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,3 +8,5 @@ WORKINGDIR=$(pwd)
 go test -v -cover --count=1 ./utils
 go test -v -cover --count=1 ./mirror
 go test -v -cover --count=1 ./image
+go test -v -cover --count=1 ./archive
+go test -v -cover --count=1 ./archive/part
