a156a13a0ec3ffe7ecc624b538d9c3ab5f71316c
Starry Wang <starry.wang@suse.com>
2023-12-04 14:32:11 +0800

    Update build flags

--- scripts/build.sh
+++ scripts/build.sh
@@ -12,29 +12,30 @@ cd $WORKINGDIR/bin
 
 echo "Start build Hangar binary..."
 
-BUILD_FLAG=""
+BUILD_ARGS="-buildmode=pie"
+BUILD_LDFAGS=""
+BUILD_TAGS=""
 if [[ -z "${DEBUG:-}" ]]; then
-    BUILD_FLAG="-extldflags -static -s -w"
+    BUILD_LDFAGS="-extldflags -static -s -w"
 else
     echo "Debug enabled for the built binary file."
 fi
 if [[ ! -z "${COMMIT}" ]]; then
-    BUILD_FLAG="${BUILD_FLAG} -X 'github.com/cnrancher/hangar/pkg/utils.GitCommit=${COMMIT}'"
+    BUILD_LDFAGS="${BUILD_LDFAGS} -X 'github.com/cnrancher/hangar/pkg/utils.GitCommit=${COMMIT}'"
 fi
-BUILD_FLAG="${BUILD_FLAG} -X 'github.com/cnrancher/hangar/pkg/utils.Version=${VERSION}'"
+BUILD_LDFAGS="${BUILD_LDFAGS} -X 'github.com/cnrancher/hangar/pkg/utils.Version=${VERSION}'"
 
 if [[ ! -z ${DISABLE_CGO:-} ]]; then
-    CGO_ENABLED=0 \
-        GOOS=$OS \
-        GOARCH=$ARCH \
-        go build \
-        -tags containers_image_openpgp \
-        -ldflags "${BUILD_FLAG}" -o hangar ..
-else
-    GOOS=$OS \
-        GOARCH=$ARCH \
-        go build -ldflags "${BUILD_FLAG}" -o hangar ..
+    export CGO_ENABLED=0
+    BUILD_TAGS="containers_image_openpgp"
 fi
+
+go build \
+    "${BUILD_ARGS}" \
+    -tags "${BUILD_TAGS}" \
+    -ldflags "${BUILD_LDFAGS}" \
+    -o hangar \
+    ${WORKINGDIR}
 
 echo "--------------------------"
 ls -alh hangar*
