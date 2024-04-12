83ae8ab6600a2203ba1a7764b9039d4fd003434a
StarryWang <starry.wang@suse.com>
2024-04-12 11:42:55 +0800

    Update generate-list validation test

--- test/suite/test_generate-list.py
+++ test/suite/test_generate-list.py
@@ -8,6 +8,17 @@ import os
 from .common import run_hangar, check
 
 
+def handle_generate_file(name):
+    f = open(name, "r")
+    print("")
+    print("Generated " + name)
+    for _ in range(0, 5):
+        print(f.readline(), end="")
+    print("......")
+    f.close()
+    os.remove(name)
+
+
 def test_generate_list_help():
     check(run_hangar(["generate-list", "--help"]))
 
@@ -18,23 +29,28 @@ def test_generate_list_gc_28():
         "--rancher=v2.8.0-ent",
         "--output=v2.8.0-ent-images.txt",
     ]))
-    f = open("v2.8.0-ent-images.txt", "r")
-    print("")
-    print("Generated image list of 'v2.8.0-ent'")
-    for _ in range(0, 5):
-        print(f.readline(), end="")
-    print("......")
-    f.close()
-    os.remove("v2.8.0-ent-images.txt")
+    handle_generate_file("v2.8.0-ent-images.txt")
+    handle_generate_file("v2.8.0-ent-versions.txt")
 
-    f = open("v2.8.0-ent-versions.txt", "r")
-    print("")
-    print("Generated k8s version list of 'v2.8.0-ent'")
-    for _ in range(0, 5):
-        print(f.readline(), end="")
-    print("......")
-    f.close()
-    os.remove("v2.8.0-ent-versions.txt")
+
+def test_generate_list_kdm_detailed_output():
+    check(run_hangar([
+        "generate-list",
+        "--rancher=v2.8.0-ent",
+        "--output=v2.8.0-ent-images.txt",
+        "--rke-images=rke-images.txt",
+        "--rke2-images=rke2-images.txt",
+        "--k3s-images=k3s-images.txt",
+        "--rke2-windows-images=rke2-windows-images.txt",
+        "--kdm-remove-deprecated=false",
+        "--auto-yes",
+    ]))
+    handle_generate_file("v2.8.0-ent-images.txt")
+    handle_generate_file("v2.8.0-ent-versions.txt")
+    handle_generate_file("rke-images.txt")
+    handle_generate_file("rke2-images.txt")
+    handle_generate_file("k3s-images.txt")
+    handle_generate_file("rke2-windows-images.txt")
 
 
 def test_generate_list_gc_28_dev():
@@ -44,23 +60,8 @@ def test_generate_list_gc_28_dev():
         "--output=v2.8.0-ent-dev-images.txt",
         "--dev"
     ]))
-    f = open("v2.8.0-ent-dev-images.txt", "r")
-    print("")
-    print("Generated image list of 'v2.8.0-ent' from DEV branch")
-    for _ in range(0, 5):
-        print(f.readline(), end="")
-    print("......")
-    f.close()
-    os.remove("v2.8.0-ent-dev-images.txt")
-
-    f = open("v2.8.0-ent-versions.txt", "r")
-    print("")
-    print("Generated k8s version list of 'v2.8.0-ent'")
-    for _ in range(0, 5):
-        print(f.readline(), end="")
-    print("......")
-    f.close()
-    os.remove("v2.8.0-ent-versions.txt")
+    handle_generate_file("v2.8.0-ent-dev-images.txt")
+    handle_generate_file("v2.8.0-ent-versions.txt")
 
 
 def test_generate_list_28():
@@ -69,23 +70,8 @@ def test_generate_list_28():
         "--rancher=v2.8.0",
         "--output=v2.8.0-images.txt",
     ]))
-    f = open("v2.8.0-images.txt", "r")
-    print("")
-    print("Generated image list of 'v2.8.0'")
-    for _ in range(0, 5):
-        print(f.readline(), end="")
-    print("......")
-    f.close()
-    os.remove("v2.8.0-images.txt")
-
-    f = open("v2.8.0-versions.txt", "r")
-    print("")
-    print("Generated k8s version list of 'v2.8.0'")
-    for _ in range(0, 5):
-        print(f.readline(), end="")
-    print("......")
-    f.close()
-    os.remove("v2.8.0-versions.txt")
+    handle_generate_file("v2.8.0-images.txt")
+    handle_generate_file("v2.8.0-versions.txt")
 
 
 def test_generate_list_28_dev():
@@ -94,20 +80,5 @@ def test_generate_list_28_dev():
         "--rancher=v2.8.0",
         "--output=v2.8.0-dev-images.txt",
     ]))
-    f = open("v2.8.0-dev-images.txt", "r")
-    print("")
-    print("Generated image list of 'v2.8.0' from DEV branch")
-    for _ in range(0, 5):
-        print(f.readline(), end="")
-    print("......")
-    f.close()
-    os.remove("v2.8.0-dev-images.txt")
-
-    f = open("v2.8.0-versions.txt", "r")
-    print("")
-    print("Generated k8s version list of 'v2.8.0'")
-    for _ in range(0, 5):
-        print(f.readline(), end="")
-    print("......")
-    f.close()
-    os.remove("v2.8.0-versions.txt")
+    handle_generate_file("v2.8.0-dev-images.txt")
+    handle_generate_file("v2.8.0-versions.txt")
